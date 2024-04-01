26917e5ef8e32ec0d8ed958daccc07aebf49969b
anuragbhanu <130905151+anuragbhanu@users.noreply.github.com>
2024-04-02 00:07:38 +0530

    Update  CSS for better responsiveness

--- src/components/View360.jsx
+++ src/components/View360.jsx
@@ -12,9 +12,9 @@ export default function View360() {
       const containerId = `container${index + 1}`;
       const viewer = new PANOLENS.Viewer({
         container: document.querySelector(`#${containerId}`),
-        autoRotate: false,
+        autoRotate: true,
         autoRotateSpeed: 0.3,
-        controlBar: false,
+        controlBar: true,
       });
       const panorama = new PANOLENS.ImagePanorama(imagePath);
       viewer.add(panorama);
@@ -26,12 +26,12 @@ export default function View360() {
       <h1 className="text-2xl md:text-3xl ml-1 pl-3 my-2 border-l-4 font-sans font-bold border-teal-400 dark:text-gray-800">
         360 Views
       </h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 bg-white">
+      <div className="grid grid-cols-1 md:grid-cols-2 m-2 p-2 bg-white">
         {imagePaths.map((_, index) => (
           <div
             key={index}
             id={`container${index + 1}`}
-            className="h-[20rem] w-[35rem] m-2 rounded-xl bg-white overflow-hidden"
+            className="w-9/10 h-[20rem] md:w-[35rem] m-4 rounded-xl bg-white overflow-hidden"
           ></div>
         ))}
       </div>
